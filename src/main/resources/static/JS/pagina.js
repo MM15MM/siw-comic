3d5e1715fea56f3d04d65b65d712636ec0c76517
MM15MM <marinabilleci01@gmail.com>
2024-02-10 08:56:16 +0100

    updates

--- src/main/resources/static/JS/pagina.js
+++ src/main/resources/static/JS/pagina.js
@@ -1,13 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Quando la pagina è completamente caricata
     document.body.style.opacity = 1; // Imposta l'opacità al valore completo
-    var header = document.querySelector("header"); // Seleziona l'elemento header
-    var footer = document.querySelector("footer"); // Seleziona l'elemento footer
-    header.style.opacity = 1; // Imposta l'opacità dell'header al valore completo
-    footer.style.opacity = 1; // Imposta l'opacità del footer al valore completo
+    var header = document.querySelector("header"); // Seleziona header
+    var footer = document.querySelector("footer"); // Seleziona footer
+    header.style.opacity = 1; // opacità dell'header al valore completo
+    footer.style.opacity = 1; // opacità del footer al valore completo
 
 });
-
+$(document).ready(function(){
+    $('.navbar-toggler').click(function(){
+      $('#navbarNav').toggleClass('show');
+    });
+  });
 // Lista degli URL delle pagine da precaricare
 var pagesToPreload = [
     '/admin/artists',
